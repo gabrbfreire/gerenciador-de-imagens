d73b928d9f35a96fb671e4bdf28ca9739da1ea85
Gabriel Freire <gabrbfreire@gmail.com>
2020-11-13 19:04:17 -0300

    Implemented React

--- backend/src/main/java/com/mvsia/desafio/controller/GreetingController.java
+++ backend/src/main/java/com/mvsia/desafio/controller/GreetingController.java
@@ -6,8 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController {
 
-    @GetMapping(value = "")
-    public String index(){
-        return "index";
-    }
+
 }
--- backend/src/main/java/com/mvsia/desafio/controller/MainController.java
+++ backend/src/main/java/com/mvsia/desafio/controller/MainController.java
@@ -2,7 +2,6 @@ package com.mvsia.desafio.controller;
 
 import com.mvsia.desafio.entity.Images;
 import com.mvsia.desafio.service.ImagesService;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
