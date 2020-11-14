eddb8d90099b26b7fb56fbf24f9847e5739ecd89
Gabriel Freire <gabrbfreire@gmail.com>
2020-11-14 18:45:58 -0300

    Bootstrap, banks SELECTS and images SELECT

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,28 +1,58 @@
-// $.ajax({
-//     type: "GET",
-//     url: "getImage",
-//     data: {id: 1},
-//     success: function (data) {
-//         console.log(data.image)
-//         let img = document.createElement("img");
-//
-//         img.src = "data:image/png;base64," + data.image;
-//         document.body.appendChild(img);
-//     }
-// })
+fetch('getImages')
+    .then(response => response.json())
+    .then(data => {
+        let images = document.getElementById("images");
+        data.forEach((item, index) => {
 
-$.ajax({
-    type: "GET",
-    url: "getImages",
-    success: function (data) {
-        console.log(data)
-        data.forEach((item, index)=>{
-            let div = document.createElement("div");
             let img = document.createElement("img");
             img.src = "data:image/png;base64," + item.image;
-            div.innerHTML = "&#10006";
-            div.appendChild(img);
-            document.body.appendChild(div);
+            //images.innerHTML = "&#10006";
+            images.appendChild(img);
         })
-    }
-})
+    });
+
+fetch('getImages')
+    .then(response => response.json())
+    .then(data => {
+        let banks = document.getElementById("banks");
+        data.forEach((item, index) => {
+
+            let li = document.createElement("li");
+            li.className = 'nav-item';
+            let a = document.createElement("a");
+            a.className = 'nav-link text-light';
+            a.href = '#';
+            a.innerHTML = 'Banco ' + item.id;
+            a.id = "bank-" + item.id;
+
+            li.appendChild(a);
+            banks.appendChild(li);
+        });
+        let li = document.createElement("li");
+        li.className = 'nav-item';
+        let a = document.createElement("a");
+        a.className = 'nav-link text-light';
+        a.href = '#';
+        a.innerHTML = 'Novo Banco';
+        a.id = 'new-bank';
+        li.appendChild(a);
+        banks.appendChild(li);
+    });
+
+/* fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+  }).then(function(response) {
+    response.status     //=> number 100–599
+    response.statusText //=> String
+    response.headers    //=> Headers
+    response.url        //=> String
+
+    return response.text()
+  }, function(error) {
+    error.message //=> String
+  }) */
